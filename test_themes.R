5d0e5a4d9d6595d561767aed87368c17dea28760
Karen Bobier <karenbobier@gmail.com>
2025-03-07 15:46:15 -0500

    Update test_themes.R
    add code to save plots

--- test_themes.R
+++ test_themes.R
@@ -4,11 +4,11 @@ library(ggthemes)
 #if you want to download just the theme file and not the repository
 download.file(url="https://raw.githubusercontent.com/karenbobier/ggplot_publication_themes/refs/heads/main/ggplot_themes_for_publications.R", destfile="ggplot_themes_for_publications.R")
 source('ggplot_themes_for_publications.R')
-source('C:/Users/karen/OneDrive/Documents/jeff_data/R_files/ggplot_themes_for_publications.R')
 
 #create a scatter plot with theme_Publication
 Scatter <- ggplot(mtcars, aes(mpg,disp,color=factor(hp))) + geom_point(size=3, stroke=1.2) + labs(title="Scatter Plot")
 Scatter + theme_Publication()
+
 #can use 21 shapes with this scale_shape_custom
 Scatter_shapes <- ggplot(mtcars, aes(mpg,disp,color=factor(hp), shape=factor(hp))) + geom_point(size=3, stroke=1.2) + labs(title="Scatter Plot")
 Scatter_shapes + theme_Publication() + scale_shape_custom()
@@ -30,3 +30,10 @@ Bar <- ggplot(mtcars, aes(factor(carb),fill=factor(carb))) +
   labs(title="Bar Plot") +theme_Publication()
 Bar 
 Bar + scale_fill_colorblind()
+
+#save plots
+ggsave("../R_files/ggplot_publication_themes/plots/scatter_pub.pdf",plot = Scatter + theme_Publication(), width=9, height=5)
+ggsave("../R_files/ggplot_publication_themes/plots/scatter_shapes_pub.pdf",plot = Scatter_shapes + theme_Publication() + scale_shape_custom(), width=9, height=5)
+ggsave("../R_files/ggplot_publication_themes/plots/scatter_cb_pub.pdf",plot = Scatter_subset + theme_Publication() + scale_colour_colorblind(), width=9, height=5)
+ggsave("../R_files/ggplot_publication_themes/plots/scatter_cb_shapes_pub.pdf",plot = Scatter_subset_shapes + theme_Publication() + scale_colour_colorblind() + scale_shape_custom(), width=9, height=5)
+ggsave("../R_files/ggplot_publication_themes/plots/bar_cb_pub.pdf",plot = Bar + scale_fill_colorblind(), width=9, height=5)
